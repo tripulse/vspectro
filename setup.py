cb3e2eba3c05980db9d0e72fbc392baec04a1b44
nullvideo <bsantanu381@gmail.com>
2019-11-13 09:26:27 +0530

    Setup script to build the Python wheel for this project

--- setup.py
+++ setup.py
@@ -2,5 +2,14 @@ import setuptools
 
 setuptools.setup(
     name= "Spectro",
-    description= "Spectro is a lightweight app for visualization of AUX audio input."
+    version= "1.4.0-beta",
+    description= "Spectro is a lightweight app for visualization of AUX audio input.",
+    long_description= open('docs/README.pypi.md').read(),
+    long_description_content_type= "text/markdown",
+    author= "nullvideo",
+    author_email= "bsantanu381@gmail.com",
+    url= "https://github.com/nullvideo/spectro",
+    packages= setuptools.find_packages(),
+    license= "MIT",
+    python_requires= ">3.6"
 )
